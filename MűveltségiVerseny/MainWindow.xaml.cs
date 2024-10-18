e5f971c399651750dcf134adf83b7198a3e2caf3
TothZsofia <tzsofia07@gmail.com>
2024-10-18 11:21:21 +0200

    Add project files.

---
+++ MűveltségiVerseny/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.Unicode;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MűveltségiVerseny
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<verseny> feladatook=new List<verseny>();
+        public MainWindow()
+        {
+            InitializeComponent();
+           using StreamReader sr = new StreamReader(
+                path: @"..\..\..\src\feladatok.txt",
+                encoding:Encoding.UTF8
+                );
+            while (!sr.EndOfStream) 
+            {
+                feladatook.Add(new verseny(sr.ReadLine()));
+            }
+            sr.Close();
+
+            //1. feladat
+            for (int i = 0; i < feladatook.Count; i++) 
+            {
+                lst2.Items.Add(feladatook[i].Kerdes);
+            }
+            
+
+        }
+
+    //2.feladat
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int szam = 0;
+            for (int i = 0; i < feladatook.Count; i++)
+            {
+                if (feladatook[i].Kerdes.Contains("?"))
+                {
+                    szam++;
+                    text.Text =$"A kérdőjeles feladatok száma: {szam}";
+                }
+            }
+
+           
+        }
+        //3.feladat
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            int szam = 0;
+            for (int i = 0; i < feladatook.Count; i++) 
+            {
+                if (feladatook[i].Terulet.Contains("kemia") && feladatook[i].Pontszam==3)
+                {
+                    szam++;
+                    
+                }
+                txt2.Text = $"Az adatfájlban {szam}db 3 pontos kémia feladat van.";
+            }
+        }
+        //4.feladat
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            var min = feladatook.Min(x => x.Valasz);
+            var max = feladatook.Max(x => x.Valasz);
+            txt3.Text = $"A válaszok értéke {min} és {max} között terjed. ";
+        }
+
+        //5.feladat
+        private void buttonotos_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> abc = new List<string>();
+            for (int i = 0; i < feladatook.Count; i++)
+            {
+                if (!abc.Contains(feladatook[i].Terulet))
+                {
+                    abc.Add(feladatook[i].Terulet);
+                }
+            }
+            abc.Sort();
+
+            foreach (var item in abc)
+            {
+                temakorbox.Items.Add(item);
+            }
+        }
+        //6.feladat
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            bool yes = false;
+            
+            for (int i = 0; i < feladatook.Count; i++) 
+            {
+                if (feladatook[i].Kerdes.Contains(aaaaaa.Text))
+                {
+                    kereseséistbox.Items.Add(feladatook[i].Kerdes);
+                    yes = true;
+                }
+               
+            }
+            if (!yes)
+            {
+                MessageBox.Show("Nincs ilyen találat!");
+            }
+            Random rnd= new Random();
+            if (kereseséistbox.Items.Count!=0)
+            {
+                int rndNext = rnd.Next(0, kereseséistbox.Items.Count);
+                bbbbbb.Content=kereseséistbox.Items[rndNext];
+            }
+
+            }
+    }
+}
---
+++ MűveltségiVerseny/verseny.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MűveltségiVerseny
+{
+    internal class verseny
+    {
+        public string Kerdes { get; set; }
+        public int Valasz { get; set; }
+        public int Pontszam { get; set; }
+        public string Terulet { get; set; }
+
+        public verseny(string sorok) 
+        {
+            var s= sorok.Split(';');
+            Kerdes = s[0];
+            Valasz = Convert.ToInt32(s[1]);
+            Pontszam = Convert.ToInt32(s[2]);
+            Terulet = s[3];
+        }    
+    }
+}
